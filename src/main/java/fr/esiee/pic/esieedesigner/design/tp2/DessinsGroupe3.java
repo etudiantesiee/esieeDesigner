6ac314346a97b92510a7374e192e9c7fe72e6d49
i.domun <irfaan.domun@edu.esiee.fr>
2016-11-28 17:03:10 +0100

    add comments on private static var

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
@@ -14,22 +14,49 @@ import fr.esiee.pic.esieedesigner.api.shapes.Ellipse;
  */
 public class DessinsGroupe3 extends CreateurDeForme {
 	
+	/**
+	 * Longueur une case
+	 */
 	private static final int PIXEL = 25;
 	
+	/**
+	 * Longueur page entière
+	 */
 	private static final int LONGUEUR_PAGE = 77 * PIXEL;
 	
+	/**
+	 * Largeur page entière
+	 */
 	private static final int LARGEUR_PAGE = 41 * PIXEL;
 	
+	/**
+	 * Largeur du personnage (cadre)
+	 */
 	private static final int LARGEUR_PERSO = 14 * PIXEL;
 	
+	/**
+	 * Longueur du personnage (cadre)
+	 */
 	private static final int LONGUEUR_PERSO = 14 * PIXEL;
-
+	
+	/**
+	 * Base Haut représente la composante y du cadre à partir du haut
+	 */
 	private static final int BASE_HAUT = LARGEUR_PAGE/2-LARGEUR_PERSO/2;
 	
+	/**
+	 * Base Bas représente la composante y du cadre à partir du bas
+	 */
 	private static final int BASE_BAS = LARGEUR_PAGE/2 + LARGEUR_PERSO/2;
 	
+	/**
+	 * Base Droite représente la composante x du cadre à partir du droite
+	 */
 	private static final int BASE_DROITE = LONGUEUR_PAGE/2 + LONGUEUR_PERSO/2;
 	
+	/**
+	 * Base Gauche représente la composante x du cadre à partir du gauche
+	 */
 	private static final int BASE_GAUCHE = LONGUEUR_PAGE/2 - LONGUEUR_PERSO/2;
 	
 	/**
