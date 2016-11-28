43f9b0f01f123fa8a2016e1e6fbf999423d63e55
v.belin <valentin.belin@edu.esiee.fr>
2016-11-28 18:25:37 +0100

    origin torse cheveux

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
@@ -22,22 +22,21 @@ public class DessinsGroupe2 extends CreateurDeForme {
 	 * Nombre de pixel d'une case
 	 */
 	private static final double CASE = 25;
+	
+	/**
+	 * Point torse_haut_gauche
+	 */
+	private static final Point TORSE_HAUT_GAUCHE = new Point(ORIGIN + CASE*3, CASE*8);
 
 	/**
 	 * Point cheveux extremité gauche
 	 */
-	private static final Point CHEVEUX_EXTREMITE_GAUCHE = new Point(ORIGIN + CASE*1, CASE*8);
+	private static final Point CHEVEUX_EXTREMITE_GAUCHE = new Point(TORSE_HAUT_GAUCHE.getX() - CASE*2, TORSE_HAUT_GAUCHE.getY());
 	
 	/**
 	 * Point joue extremité gauche exterieur
 	 */
 	private static final Point JOUE_EXTREMITE_GAUCHE_EXTERIEUR = new Point(ORIGIN + CASE*3, CASE*7);
-	
-	/**
-
-	 * Point torse_haut_gauche
-	 */
-	private static final Point TORSE_HAUT_GAUCHE = new Point(ORIGIN + CASE*3, CASE*8);
 	
 	/**
 	 * Point joue extremité droite exterieur
@@ -48,7 +47,6 @@ public class DessinsGroupe2 extends CreateurDeForme {
 	 * Point fixe pour dessiner le nez
 	 */
 	private static final Point NEZ_ORIGINE = new Point(ORIGIN + CASE * 6.8, CASE * 5.2);
-
 	
 	/**
 	 * Point fixe pour dessiner le premier oeil (gauche)
