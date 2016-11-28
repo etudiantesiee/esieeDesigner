7f10e723e0265b0785d3ab4b3892aed445cce25e
lti <landry.tiehi@gmail.com>
2016-11-28 15:37:08 +0100

    gp1 cheveux

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
@@ -78,15 +78,37 @@ public class DessinsGroupe1 extends CreateurDeForme {
 	 * Thomas et Eve
 	 */
 	public void dessinerCheveux() {
+		Point pointeGauche1 = new Point((ORIGIN_X + 3) * UNITE_HORIZONTALE, (ORIGIN_Y + 4) * UNITE_VERTICALE);
+		Point pointeGauche2 = new Point((ORIGIN_X + 3) * UNITE_HORIZONTALE, (ORIGIN_Y + 6) * UNITE_VERTICALE);
+		Point pointeGauche3 = new Point((ORIGIN_X + 2) * UNITE_HORIZONTALE, (ORIGIN_Y + 7) * UNITE_VERTICALE);
+		Point pointeGauche4 = new Point((ORIGIN_X + 1) * UNITE_HORIZONTALE, (ORIGIN_Y + 7) * UNITE_VERTICALE);
 		Point touffeGaucheMilieu = new Point(ORIGIN_X * UNITE_HORIZONTALE, (ORIGIN_Y + 3) * UNITE_VERTICALE);
 		Point touffeGauche1 = new Point(ORIGIN_X * UNITE_HORIZONTALE, (ORIGIN_Y + 1) * UNITE_VERTICALE);
 		Point touffeGauche2 = new Point((ORIGIN_X + 1) * UNITE_HORIZONTALE, ORIGIN_Y * UNITE_VERTICALE);
+		Point touffeDroite1 = new Point((ORIGIN_X + 13) * UNITE_HORIZONTALE, ORIGIN_Y * UNITE_VERTICALE);
+		Point touffeDroite2 = new Point((ORIGIN_X + 14) * UNITE_HORIZONTALE, (ORIGIN_Y + 1) * UNITE_VERTICALE);
+		Point touffeDroiteMilieu = new Point((ORIGIN_X + 14) * UNITE_HORIZONTALE, (ORIGIN_Y + 3) * UNITE_VERTICALE);
+		Point pointeDroite1 = new Point((ORIGIN_X + 13) * UNITE_HORIZONTALE, (ORIGIN_Y + 7) * UNITE_VERTICALE);
+		Point pointeDroite2 = new Point((ORIGIN_X + 12) * UNITE_HORIZONTALE, (ORIGIN_Y + 7) * UNITE_VERTICALE);
+		Point pointeDroite3 = new Point((ORIGIN_X + 11) * UNITE_HORIZONTALE, (ORIGIN_Y + 6) * UNITE_VERTICALE);
+		Point pointeDroite4 = new Point((ORIGIN_X + 11) * UNITE_HORIZONTALE, (ORIGIN_Y + 4) * UNITE_VERTICALE);
 		
 		
 		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(pointeGauche1)
+		.ajouter(pointeGauche2)
+		.ajouter(pointeGauche3)
+		.ajouter(pointeGauche4)
 		.ajouter(touffeGaucheMilieu)
 		.ajouter(touffeGauche1)
-		.ajouter(touffeGauche2);
+		.ajouter(touffeGauche2)
+		.ajouter(touffeDroite1)
+		.ajouter(touffeDroite2)
+		.ajouter(touffeDroiteMilieu)
+		.ajouter(pointeDroite1)
+		.ajouter(pointeDroite2)
+		.ajouter(pointeDroite3)
+		.ajouter(pointeDroite4);
 		
 	}
 	
