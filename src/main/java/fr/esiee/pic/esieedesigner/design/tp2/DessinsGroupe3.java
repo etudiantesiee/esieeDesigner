4fc64af3dcef8543042886bad5f8c6c5f42cbf91
i.domun <irfaan.domun@edu.esiee.fr>
2016-11-28 16:27:05 +0100

    Commit 2 bras Droit

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
@@ -1,6 +1,7 @@
 package fr.esiee.pic.esieedesigner.design.tp2;
 
 import fr.esiee.pic.esieedesigner.api.tools.CreateurDeForme;
+import fr.esiee.pic.esieedesigner.api.shapes.*;
 
 /**
  * Le groupe 3 ajoutera ses dessins dans cette classe.
@@ -33,13 +34,22 @@ public class DessinsGroupe3 extends CreateurDeForme {
 		// Ajout vos dessins ici
 		// Pensez à désactiver les dessins de la démo 
 		// en commentant initDemo dans le méthode init()de la classe fr.esiee.pic.esieedesigner.ui.scene.EcranPrincipal
-		
+		dessineBrasDroit();
 	}
 	
 	/**
 	 * @author mathieu
 	 * Dessine le bras Droit
 	 */
-	public void DessineBrasDroit(){
+	public void dessineBrasDroit(){
+		
+		Point epaule = new Point(BASE_DROITE - 3*PIXEL, BASE_BAS - 6*PIXEL);
+		Point coude = new Point(BASE_DROITE - 2*PIXEL, BASE_BAS - 4*PIXEL);
+		
+		// On ajoute à la liste des composants à déssiner
+    	demarrerNouveauDessinAvecDesPoints()
+        .ajouter(epaule)
+        .ajouter(coude);
+		
 	}
 }
