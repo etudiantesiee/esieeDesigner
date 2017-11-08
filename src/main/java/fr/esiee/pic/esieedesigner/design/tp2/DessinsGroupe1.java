f9a58c289d871563749724acadc97c5c3edeb64f
v.portais <valentin.portais@edu.esiee.fr>
2017-11-08 10:13:39 +0100

    Ajout des premiers points de la tete

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
@@ -1,5 +1,6 @@
 package fr.esiee.pic.esieedesigner.design.tp2;
 
+import fr.esiee.pic.esieedesigner.api.shapes.Point;
 import fr.esiee.pic.esieedesigner.api.tools.CreateurDeForme;
 
 /**
@@ -26,10 +27,27 @@ public class DessinsGroupe1 extends CreateurDeForme {
 		// Ajout vos dessins ici
 		// Pensez à désactiver les dessins de la démo 
 		// en commentant initDemo dans le méthode init()de la classe fr.esiee.pic.esieedesigner.ui.scene.EcranPrincipal
+		dessinerTete();
+	
 	}
 	
 	public void dessinerTete(){
+		double X = UNITE_VERTICALE/2;
+		double Y = UNITE_HORIZONTALE/2;
 		
+		Point a = new Point (4*X, 0);
+		Point b = new Point (10*X, 0);
+		Point c = new Point (12*X, 2*Y);
+		Point d = new Point (13*X, 5*Y);
+		Point e = new Point (9*X, 1*Y);
+		Point f = new Point (7*X, 2*Y);
+		Point g = new Point (5*X, 1*Y);
+		
+		
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(a)
+		.ajouter(b)
+		.nePasRelierLesPointsExtreme();
 	
 	}
 	
