4abeeaea9c3fa334cb95a6998fadf3ca02798554
gilles.laroussi <gilles.laroussi@edu.esiee.fr>
2017-11-08 10:22:14 +0100

    Grp2 : plat du pied

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
@@ -29,13 +29,27 @@ public class DessinsGroupe2 extends CreateurDeForme {
 	 * Permet de connaitre la taille de déplacement en Y de la figure
 	 * Exemple : Il doit être à 28 (2 x 14) pour l'étape 1
 	 */
-	private static final int decalageEnY = 28;
+	private static final int decalageEnY = 0;
 	
 	@Override
 	public void dessiner() {
 		
+		//Dessin du corps
+		dessinCorps();
+		
+		
 	}
 	
+	private void dessinCorps(){
+		Point orteilGauche = new Point(vraiePositionEnX(12),vraiePositionEnY(14));
+		Point talonGauche = new Point(vraiePositionEnX(10),vraiePositionEnY(14));
+
+		
+		demarrerNouveauDessinAvecDesPoints()
+		.ajouter(orteilGauche)
+		.ajouter(talonGauche);
+		
+	}
 	
 	private void suitePointDessin(Point[] points){
 		CheminDePoints cdp = demarrerNouveauDessinAvecDesPoints();
