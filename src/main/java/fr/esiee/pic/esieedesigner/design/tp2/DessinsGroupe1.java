d91875bf0477aa7bdb0bd834fdf034f0a2fc40ed
a.guerrida <asma.guerrida@edu.esiee.fr>
2017-11-08 10:41:12 +0100

    commit3

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
@@ -97,14 +97,20 @@ public class DessinsGroupe1 extends CreateurDeForme {
 		//création des points pour le BRAS GAUCHE
 		Point p3 = new Point(1*UNITE_HORIZONTALE,12*UNITE_VERTICALE);
 		Point p4 = new Point(2*UNITE_HORIZONTALE,12*UNITE_VERTICALE);
+		//création des points pour l'aisselle gauche
+		Point p5 = new Point(2*UNITE_HORIZONTALE,7*UNITE_VERTICALE);
+		Point p6 = new Point(3*UNITE_HORIZONTALE,11*UNITE_VERTICALE);
 		//RELIER LES 2 POINTS DE L'ÉPAULE
 		demarrerNouveauDessinAvecDesPoints()
 		.ajouter(p1)
 		.ajouter(p2)
 		//RELIER LES 2 POINTS DU BRAS
 		.ajouter(p3)
-		.ajouter(p4);
-		
+		.ajouter(p4)
+		//RELIER LES 2 POINTS De l'aisselle gauche
+	     .ajouter(p5)
+	     .ajouter(p6)
+		.nePasRelierLesPointsExtreme();
 		
 		
 			
