8051428d0a02d964a7dbd992ad6e3a2802ed08b4
YanGuillaume <YanFDP@mail.fr>
2017-11-08 10:50:39 +0100

    point 7 8

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe3.java
@@ -31,10 +31,12 @@ public class DessinsGroupe3 extends CreateurDeForme {
 			Point unG = new Point(menton.getX()-75, menton.getY()-50);
 			Point deuxG = new Point(unG.getX()-25, unG.getY()-75);
 			Point troisG = new Point(deuxG.getX(), deuxG.getY()-50);
-			Point quatreG = new Point(troisG.getX(), troisG.getY()-50);
-			Point cinqG = new Point(quatreG.getX()+75, quatreG.getY()-25);
-			Point sixG = new Point(cinqG.getX()+25, cinqG.getY()+25);
-			
+			Point quatreG = new Point(troisG.getX()+75, troisG.getY()-25);
+			Point cinqG = new Point(quatreG.getX()+25, quatreG.getY()+25);
+			Point septD = new Point(cinqG.getX()+25, cinqG.getY()-25);
+			Point huitD = new Point(septD.getX()+75, septD.getY()+25);
+			Point neufD = new Point(huitD.getX(), huitD.getY()+50);
+			Point dixD = new Point(neufD.getX()-25, neufD.getY()+75);
 	
 			demarrerNouveauDessinAvecDesPoints()
 			.ajouter(menton)
@@ -43,7 +45,10 @@ public class DessinsGroupe3 extends CreateurDeForme {
 			.ajouter(troisG)
 			.ajouter(quatreG)
 			.ajouter(cinqG)
-			.ajouter(sixG);
+			.ajouter(septD)
+			.ajouter(huitD)
+			.ajouter(neufD)
+			.ajouter(dixD);
 
 		}
 }
