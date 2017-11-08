669c97454cfda342098ca65e11ed42b25d4e1340
j.fornarino <jeremy.fornarino@edu.esiee.fr>
2017-11-08 09:55:03 +0100

    Initialisation des premieres fonctions

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
@@ -1,20 +1,82 @@
 package fr.esiee.pic.esieedesigner.design.tp2;
 
+import fr.esiee.pic.esieedesigner.api.shapes.Point;
 import fr.esiee.pic.esieedesigner.api.tools.CreateurDeForme;
+import fr.esiee.pic.esieedesigner.api.shapes.*;
 
 /**
  * Le groupe 2 ajoutera ses dessins dans cette classe.
- * 
- * @author etudiant
- *
+ * @author Nuzha
+ * @author Marwa
+ * @author Giles
+ * @author Lamia
+ * @author Jérémy
+ * @date 2017
  */
 public class DessinsGroupe2 extends CreateurDeForme {
-
+	/**
+	 * La variable représente la taille d'un carreau
+	 * elle permet de calculer la taille en pixel
+	 * Utilisé dans les méthode {@link vraieTailleEnX} et {@link vraieTailleEnY}
+	 */
+	private static final int nbrPixelCarreau = 25;
+	/** 
+	 * Permet de connaitre la taille de déplacement en X de la figure
+	 * Exemple : Il doit être à 28 (2 x 14) pour l'étape 1
+	 */
+	private static final int decalageEnX = 28;
+	/** 
+	 * Permet de connaitre la taille de déplacement en Y de la figure
+	 * Exemple : Il doit être à 28 (2 x 14) pour l'étape 1
+	 */
+	private static final int decalageEnY = 28;
+	
 	@Override
 	public void dessiner() {
-		// Ajout vos dessins ici
-		// Pensez à désactiver les dessins de la démo 
-		// en commentant initDemo dans le méthode init()de la classe fr.esiee.pic.esieedesigner.ui.scene.EcranPrincipal
+		
 	}
-
+	
+	
+	private void suitePointDessin(Point[] points){
+		CheminDePoints cdp = demarrerNouveauDessinAvecDesPoints();
+		for(int i = 0; i < points.length; i++){
+			cdp.ajouter(points[i]);
+		}
+	}
+	/**
+	 * Permet de connaitre la vraie position en X
+	 * Pour un nbCarreau entier donné
+	 * @param nbCarreau Entier indiquant le décalage vers la droite (en X)
+	 * @return La réel position en X, comprenant le décalage et le nombre de pixel
+	 */
+	private int vraiePositionEnX(int nbCarreau){
+		return (DessinsGroupe2.decalageEnX + nbCarreau)*DessinsGroupe2.nbrPixelCarreau;
+	}
+	/**
+	 * Permet de connaitre la vraie position en X
+	 * Pour un nbCarreau double donné
+	 * @param nbCarreau double indiquant le décalage vers la droite (en X)
+	 * @return La réel position en X, comprenant le décalage et le nombre de pixel
+	 */
+	private double vraiePositionEnX(double nbCarreau){
+		return (DessinsGroupe2.decalageEnX + nbCarreau)*DessinsGroupe2.nbrPixelCarreau;
+	}
+	/**
+	 * Permet de connaitre la vraie position en Y
+	 * Pour un nbCarreau entier donné
+	 * @param nbCarreau entier indiquant le décalage vers le bas (en Y)
+	 * @return La réel position en Y, comprenant le décalage et le nombre de pixel
+	 */
+	private int vraiePositionEnY(int nbCarreau){
+		return (DessinsGroupe2.decalageEnY + nbCarreau)*DessinsGroupe2.nbrPixelCarreau;
+	}
+	/**
+	 * Permet de connaitre la vraie position en Y
+	 * Pour un nbCarreau double donné
+	 * @param nbCarreau double indiquant le décalage vers le bas (en Y)
+	 * @return La réel position en Y, comprenant le décalage et le nombre de pixel
+	 */
+	private double vraiePositionEnY(double nbCarreau){
+		return (DessinsGroupe2.decalageEnY + nbCarreau)*DessinsGroupe2.nbrPixelCarreau;
+	}
 }
